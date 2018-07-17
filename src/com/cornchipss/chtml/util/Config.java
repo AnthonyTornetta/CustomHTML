2e855b76d397a890bc02596c19eb5cf5ea89224f
Cornchip <cornchipgonecodin@gmail.com>
2018-07-17 16:07:47 -0400

    Rewrote the config file class
    Way more optimized
    Now using HashMaps instead of iterating over every line and parsing it every time
    Also now with full comment support!

--- src/com/cornchipss/chtml/util/Config.java
+++ src/com/cornchipss/chtml/util/Config.java
@@ -1,57 +1,146 @@
 package com.cornchipss.chtml.util;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Config 
+public class Config
 {
-	public static final int BAD_VALUE = -9999;
-	
+	/**
+	 * The file to be saved and read from
+	 */
 	private File configFile;
 	
-	ArrayList<String> lines = new ArrayList<>();
+	/**
+	 * Keeps track of comments based of things respective keys so when the config is saved, the comments aren't lost
+	 */
+	private Map<String, String> comments = new HashMap<>();
 	
 	/**
-	 * Reads and rights to a config file using the following format:
-	 * <br>
-	 * [key]: [data]
-	 * <br>
-	 * The config file will be created if it doesn't exist
-	 * <br>
-	 * The file saving format is of the .yml file type, thus files should be saved with a .yml extension, but you can make the extension anything you want
-	 * @param cfgFile The file to read/write from
+	 * Stores every Integer in relation with their key
+	 */
+	private Map<String, Integer> ints = new HashMap<>();
+	
+	/**
+	 * Stores every String in relation with their key
+	 */
+	private Map<String, String> strings = new HashMap<>();
+	
+	/**
+	 * Stores every Double in relation with their key
+	 */
+	private Map<String, Double> doubles = new HashMap<>();
+	
+	/**
+	 * Stores every Double array in relation with their key
+	 */
+	private Map<String, Double[]> doubleArrays = new HashMap<>();
+	
+	/**
+	 * Stores every Integer array in relation with their key
+	 */
+	private Map<String, Integer[]> intArrays = new HashMap<>();
+	
+	public static final char COMMENT_CHAR = '#';
+		
+	/**
+	 * <p>Reads and rights to a config file using the following format: <code>[key]: [data]</code></p>
+	 * <p>Comments should be started on a seperate line from values, and the first character on that line must be a '#' character.</p>
+	 * <p>The config file will be created if it doesn't exist.</p>
+	 * <p>The file saving format is of the .yml file type.</p>
+	 * @param path The path to the config file. If no file exists, it will be created when save() is called.
 	 */
 	public Config(String path)
 	{
 		try
 		{
 			configFile = new File(path);
-			if(!configFile.exists())
+			if(configFile.exists())
 			{
-				configFile.createNewFile();
+				BufferedReader br = new BufferedReader(new FileReader(configFile));
+			    
+			    // Preserves comments in the config file
+			    StringBuilder commentStringBuilder = new StringBuilder();
+			    
+			    // Read all the lines and put them into the array list
+			    for(String line = br.readLine(); line != null; line = br.readLine())
+			    {
+		        	if(line.length() >= 1)
+		        	{
+		        		if(Helper.removeTrailingWhiteSpace(line).charAt(0) != COMMENT_CHAR)
+		        		{
+							String[] split = line.split(": ");
+							if(split.length == 1)
+								split = line.split(":"); // Check if they just had no space after the colon
+							
+							if(split.length > 1) // This is a thing we should parse
+							{
+								if(Helper.isInt(split[1]))
+								{
+									ints.put(split[0], Integer.parseInt(split[1]));
+								}
+								else if(Helper.isDouble(split[1]))
+								{
+									doubles.put(split[0], Double.parseDouble(split[1]));
+								}
+								else
+								{
+									boolean isIntArray = true;
+									boolean isDoubleArray = true;
+									
+									// Check if it's a number array
+									String[] superSplit = split[1].split(",");
+									for(int i = 0; i < superSplit.length; i++)
+									{
+										superSplit[i] = Helper.removeTrailingWhiteSpace(superSplit[i]);
+										if(!Helper.isInt(superSplit[i]))
+											isIntArray = false;
+										if(!isIntArray && !Helper.isDouble(superSplit[i]))
+											isDoubleArray = false;
+									}
+									
+									if(isIntArray)
+									{
+										Integer[] arr = new Integer[superSplit.length];
+										for(int i = 0; i < superSplit.length; i++)
+										{
+											arr[i] = Integer.parseInt(superSplit[i]);
+										}
+										
+										intArrays.put(split[0], arr);
+									}
+									else if(isDoubleArray)
+									{
+										Double[] arr = new Double[superSplit.length];
+										for(int i = 0; i < superSplit.length; i++)
+										{
+											arr[i] = Double.parseDouble(superSplit[i]);
+										}
+										
+										doubleArrays.put(split[0], arr);
+									}
+									else
+										strings.put(split[0], split[1]); // If all else fails, it's a String
+								}
+								
+								// Puts comments in by the key they were behind
+								comments.put(split[0], commentStringBuilder.toString());
+								commentStringBuilder.setLength(0); // Clears the string builder
+							}
+		        		}
+		        		else
+		        		{
+	    	        		commentStringBuilder.append(line + "\r\n");
+		        		}
+		        	}
+			    }
+			    br.close();
 			}
-		    InputStream fis = new FileInputStream(configFile);
-		    InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
-		    BufferedReader br = new BufferedReader(isr);
-		    
-		    // Read all the lines and put them into the array list
-		    for(String line = br.readLine(); line != null; line = br.readLine())
-		    {
-		        lines.add(line);
-		    }
-		    br.close();
-		} 
-		catch (FileNotFoundException ex) 
-		{
-			ex.printStackTrace();
 		} 
 		catch (IOException ex) 
 		{
@@ -62,21 +151,40 @@ public class Config
 	/**
 	 * Sets a integer value at a given key
 	 * @param key The key to set the value at
-	 * @param integer The value the integer will have
+	 * @param integer The value the key will store
 	 */
 	public void setInt(String key, int integer)
 	{
-		String newValue = key + ": " + integer;
-		for(int i = 0; i < lines.size(); i++)
-		{
-			String[] split = lines.get(i).split(": ");
-			if(split[0].equalsIgnoreCase(key))
-			{
-				lines.set(i, newValue);
-				return;
-			}
-		}
-		lines.add(newValue);
+		ints.put(key, integer);
+	}
+	
+	/**
+	 * Gets the Integer at the specified key
+	 * @param key The key to get the Integer at
+	 * @throws IllegalArgumentException if there is no key present
+	 * @return The Integer at the specified key
+	 */
+	public int getInt(String key)
+	{
+		if(!ints.containsKey(key))
+			throw new IllegalArgumentException("Key of " + key + " not found!");
+		
+		return ints.get(key);
+	}
+	
+	/**
+	 * Gets an Integer value from the config file if it exists, or sets the value if it doesn't
+	 * @param key The key to access or set
+	 * @param defaultVal The value to set the key to if the value is not present
+	 * @return The value present if it exists, or defaultVal if it wasn't present
+	 */
+	public int getOrSetInt(String key, int defaultVal)
+	{
+		if(ints.containsKey(key))
+			return getInt(key);
+		
+		setInt(key, defaultVal);
+		return defaultVal;
 	}
 	
 	/**
@@ -86,109 +194,7 @@ public class Config
 	 */
 	public void setDouble(String key, double d)
 	{
-		String newValue = key + ": " + d;
-		for(int i = 0; i < lines.size(); i++)
-		{
-			String[] split = lines.get(i).split(": ");
-			if(split[0].equalsIgnoreCase(key))
-			{
-				lines.set(i, newValue);
-				return;
-			}
-		}
-		lines.add(newValue);
-	}
-	
-	/**
-	 * Sets a String value at a given key
-	 * @param key The key to set the value at
-	 * @param str The value the String will have
-	 */
-	public void setString(String key, String str)
-	{
-		String newValue;
-		if(str == null)
-			newValue = key + ": null";
-		else
-			newValue = key + ": " + str;
-		
-		for(int i = 0; i < lines.size(); i++)
-		{
-			String[] split = lines.get(i).split(": ");
-			if(split[0].equalsIgnoreCase(key))
-			{
-				lines.set(i, newValue);
-				return;
-			}
-		}
-		lines.add(newValue);
-	}
-	
-	/**
-	 * Checks if the configuration file contains a key
-	 * @param key The key to check if it exists
-	 * @return True if it exists; False if it doesn't
-	 */
-	public boolean containsKey(String key)
-	{
-		for(int i = 0; i < lines.size(); i++)
-		{
-			String[] split = lines.get(i).split(": ");
-			if(split[0].equalsIgnoreCase(key))
-				return true;
-		}
-		return false;
-	}
-	
-	/**
-	 * Gets a String value from the config file<br>Returns BAD_VALUE if the key isn't found
-	 * @param key The key the value is stored at
-	 * @return The String that was found or BAD_VALUE if it wasn't found
-	 */
-	public String getString(String key)
-	{
-		for(int i = 0; i < lines.size(); i++)
-		{
-			String[] split = lines.get(i).split(": ");
-			if(split[0].equalsIgnoreCase(key))
-			{
-				if(split.length > 1)
-				{
-					String strToReturn = "";
-					for(int j = 1; j < split.length; j++)
-					{
-						strToReturn += split[j];
-					}
-					return strToReturn;
-				}
-			}
-		}
-		return null;
-	}
-	
-	/**
-	 * Gets a integer value from the config file<br>Returns BAD_VALUE if the key isn't found
-	 * @param key The key the value is stored at
-	 * @return The integer that was found or BAD_VALUE if it wasn't found
-	 */
-	public int getInt(String key)
-	{
-		for(int i = 0; i < lines.size(); i++)
-		{
-			String[] split = lines.get(i).split(": ");
-			if(split[0].equalsIgnoreCase(key))
-			{
-				if(split.length > 1)
-				{
-					String possibleInt = split[1];
-					if(Helper.isInt(possibleInt))
-					{
-						return Integer.parseInt(possibleInt);
-					}
-				}
-			}
-		}
-		return BAD_VALUE;
+		doubles.put(key, d);
 	}
 	
 	/**
@@ -198,44 +204,211 @@ public class Config
 	 */
 	public double getDouble(String key)
 	{
-		for(int i = 0; i < lines.size(); i++)
-		{
-			String[] split = lines.get(i).split(": ");
-			if(split[0].equalsIgnoreCase(key))
-			{
-				if(split.length > 1)
-				{
-					String possibleInt = split[1];
-					if(Helper.isDouble(possibleInt))
-					{
-						return Double.parseDouble(possibleInt);
-					}
-				}
-			}
-		}
-		return BAD_VALUE;
+		if(!doubles.containsKey(key))
+			throw new IllegalArgumentException("Key of " + key + " not found!");
+		
+		return doubles.get(key);
 	}
 	
 	/**
-	 * Saves the configuration file
+	 * Gets a Double value from the config file if it exists, or sets the value if it doesn't
+	 * @param key The key to access or set
+	 * @param defaultVal The value to set the key to if the value is not present
+	 * @return The value present if it exists, or defaultVal if it wasn't present
+	 */
+	public double getOrSetDouble(String key, double defaultVal)
+	{
+		if(doubles.containsKey(key))
+			return getDouble(key);
+		
+		setDouble(key, defaultVal);
+		return defaultVal;
+	}
+	
+	/**
+	 * Sets a String value at a given key
+	 * @param key The key to set the value at
+	 * @param str The value the String will have
+	 */
+	public void setString(String key, String str)
+	{
+		strings.put(key, str);
+	}
+	
+	/**
+	 * Gets a String value from the config file<br>Returns BAD_VALUE if the key isn't found
+	 * @param key The key the value is stored at
+	 * @return The String that was found or BAD_VALUE if it wasn't found
+	 */
+	public String getString(String key)
+	{
+		if(!strings.containsKey(key))
+			throw new IllegalArgumentException("Key of " + key + " not found!");
+		
+		return strings.get(key);
+	}
+	
+	/**
+	 * Gets a String value from the config file if it exists, or sets the value if it doesn't
+	 * @param key The key to access or set
+	 * @param defaultVal The value to set the key to if the value is not present
+	 * @return The value present if it exists, or defaultVal if it wasn't present
+	 */
+	public String getOrSetString(String key, String defaultVal)
+	{
+		if(strings.containsKey(key))
+			return getString(key);
+		
+		setString(key, defaultVal);
+		return defaultVal;
+	}
+	
+	/**
+	 * Sets a String value at a given key
+	 * @param key The key to set the value at
+	 * @param str The value the String will have
+	 */
+	public void setIntegerArray(String key, Integer[] arr)
+	{
+		intArrays.put(key, arr);
+	}
+	
+	/**
+	 * Gets a String value from the config file<br>Returns BAD_VALUE if the key isn't found
+	 * @param key The key the value is stored at
+	 * @return The String that was found or BAD_VALUE if it wasn't found
+	 */
+	public Integer[] getIntegerArray(String key)
+	{
+		if(!intArrays.containsKey(key))
+			throw new IllegalArgumentException("Key of " + key + " not found!");
+		
+		return intArrays.get(key);
+	}
+	
+	/**
+	 * Gets an Integer Array from the config file if it exists, or sets the value if it doesn't
+	 * @param key The key to access or set
+	 * @param defaultVal The value to set the key to if the value is not present
+	 * @return The value present if it exists, or defaultVal if it wasn't present
+	 */
+	public Integer[] getOrSetIntegerArray(String key, Integer[] defaultVal)
+	{
+		if(strings.containsKey(key))
+			return getIntegerArray(key);
+		
+		setIntegerArray(key, defaultVal);
+		return defaultVal;
+	}
+	
+	/**
+	 * Sets a String value at a given key
+	 * @param key The key to set the value at
+	 * @param str The value the String will have
+	 */
+	public void setDoubleArray(String key, Double[] arr)
+	{
+		doubleArrays.put(key, arr);
+	}
+	
+	/**
+	 * Gets a String value from the config file<br>Returns BAD_VALUE if the key isn't found
+	 * @param key The key the value is stored at
+	 * @return The String that was found or BAD_VALUE if it wasn't found
+	 */
+	public Double[] getDoubleArray(String key)
+	{
+		if(!doubleArrays.containsKey(key))
+			throw new IllegalArgumentException("Key of " + key + " not found!");
+		
+		return doubleArrays.get(key);
+	}
+	
+	/**
+	 * Gets an Integer Array from the config file if it exists, or sets the value if it doesn't
+	 * @param key The key to access or set
+	 * @param defaultVal The value to set the key to if the value is not present
+	 * @return The value present if it exists, or defaultVal if it wasn't present
+	 */
+	public Double[] getOrSetDoubleArray(String key, Double[] defaultVal)
+	{
+		if(doubles.containsKey(key))
+			return getDoubleArray(key);
+		
+		setDoubleArray(key, defaultVal);
+		return defaultVal;
+	}
+	
+	/**
+	 * Checks if the configuration file contains a key
+	 * @param key The key to check if it exists
+	 * @return True if it exists; False if it doesn't
+	 */
+	public boolean containsKey(String key)
+	{
+		return strings.containsKey(key) || doubles.containsKey(key) || ints.containsKey(key) || intArrays.containsKey(key) || doubleArrays.containsKey(key);
+	}
+	
+	/**
+	 * Saves the configuration file according to how yml files are formatted
 	 * <br>
 	 * Note: The config file can be saved multiple times, and there is no need to close it, just call this method to save it
 	 * @throws IOException If there was some sort of error saving the file
 	 */
 	public void save() throws IOException
 	{
+		new File(configFile.getAbsolutePath().substring(0, configFile.getAbsolutePath().lastIndexOf("\\"))).mkdirs();
 		configFile.createNewFile();
-		PrintWriter pw = new PrintWriter(configFile);
-				
-		pw.write("");
-		for(int i = 0; i < lines.size(); i++)
+		
+		BufferedWriter bw = new BufferedWriter(new FileWriter(configFile));
+		
+		bw.write("");
+		
+		for(String key : ints.keySet())
 		{
-			String curLine = lines.get(i);
-			System.out.println(curLine);
-			pw.append(curLine);
-			
-			pw.append('\n');
+			if(comments.containsKey(key))
+				bw.append(comments.get(key));
+			bw.append(key + ": " + ints.get(key) + "\r\n");
 		}
-		pw.close();
+		
+		for(String key : doubles.keySet())
+		{
+			if(comments.containsKey(key))
+				bw.append(comments.get(key));
+			bw.append(key + ": " + doubles.get(key) + "\r\n");
+		}
+		
+		for(String key : strings.keySet())
+		{
+			if(comments.containsKey(key))
+				bw.append(comments.get(key));
+			bw.append(key + ": " + strings.get(key) + "\r\n");
+		}
+		
+		for(String key : intArrays.keySet())
+		{
+			if(comments.containsKey(key))
+				bw.append(comments.get(key));
+			bw.append(key + ": ");
+			for(int i = 0; i < intArrays.get(key).length; i++)
+			{
+				bw.append(intArrays.get(key)[i] + (i + 1 != intArrays.get(key).length ? ", " : ""));
+			}
+			bw.append("\r\n");
+		}
+		
+		for(String key : doubleArrays.keySet())
+		{
+			if(comments.containsKey(key))
+				bw.append(comments.get(key));
+			bw.append(key + ": ");
+			for(int i = 0; i < doubleArrays.get(key).length; i++)
+			{
+				bw.append(doubleArrays.get(key)[i] + (i + 1 != doubleArrays.get(key).length ? ", " : ""));
+			}
+			bw.append("\r\n");
+		}
+		
+		bw.close();
 	}
 }
